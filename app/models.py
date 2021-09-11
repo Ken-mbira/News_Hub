6ed1abad3a87f81c4ade44285004ccbc272dd2aa
ken <ken.mbira@student.moringaschool.com>
2021-09-11 10:49:13 +0300

    add tests for the source class and add the article class and its tests

--- app/models.py
+++ app/models.py
@@ -18,3 +18,36 @@ class Source:
         self.description = description
         self.category = category
         self.country = country
+
+class Article:
+    """
+    This will define everything about a news article
+    """
+    def __init__(self,source,author,title,description,link,image_url,published):
+        """
+        This will define all the properties in the article class
+        
+        Args:
+            source: This is the name of the news source
+            author: This is the individual who wrote the news article
+            title: This is the heading of the news article
+            description: This is the brief summary of the article
+            link: This is the direct link to the source of the news
+            image_url: This is the link to the image attached to the article
+            published: This is the date of publication
+        """
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.link = link
+        self.image_url = image_url
+        self.published = published
+
+    article_findings = []
+
+    def save_article(self):
+        """
+        This function will add an article to the article_findings array
+        """
+        self.article_findings.append(self)
---
+++ test_source.py
@@ -0,0 +1,51 @@
+import unittest
+
+from app.models import Source,Article
+
+class SourceTest(unittest.TestCase):
+    """
+    This will test all the behaviours of the Source class
+    
+    Args: 
+        unittest.TestCase: This is a class in the unittest module where we inherit the testing tools
+    """
+    def setUp(self):
+        """
+        This function will run before every test case does
+        """
+        self.new_source = Source("cnn",'CNN','Your dedicated news source','general','US')
+
+    def test_init(self):
+        """
+        This will test whether a source object is initialized correctly
+        """
+        self.assertTrue(isinstance(self.new_source,Source))
+
+class ArticleTest(unittest.TestCase):
+    """
+    This will test all the behaviours of the Article class
+    
+    Args: 
+        unittest.TestCase: This is a class in the unittest module where we inherit the testing tools
+    """
+    def setUp(self):
+        """
+        This will run before every test in the class is run
+        """
+        self.new_article = Article('cnn','Ken Mbira','Blinding lights','A tale of two dollars','htttps://blah','link to image','2/4/5')
+
+    def test_init(self):
+        """
+        This will check if the article objects are being instantiated correctly
+        """
+        self.assertTrue(isinstance(self.new_article,Article))
+
+    def test_save_article(self):
+        """
+        This test will check if we can save a news article
+        """
+        self.new_article.save_article()
+        self.assertEqual(len(Article.article_findings),1)
+
+if __name__ == '__main__':
+    unittest.main()
