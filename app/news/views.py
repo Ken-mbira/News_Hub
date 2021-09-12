61b20b6113fb3ad943285bfd17a78376e8cc8bf8
ken <ken.mbira@student.moringaschool.com>
2021-09-12 13:49:13 +0300

    add a sources section to show the main news sources and a brief description as well as added a link to google fonts

--- app/news/views.py
+++ app/news/views.py
@@ -1,9 +1,9 @@
 from . import news
 from flask import render_template
-from ..requests import get_headlines  
+from ..requests import get_sources  
 
 @news.route('/')
 def index():
     title = 'NEWS_HUB'
-    articles = get_headlines()
-    return render_template('index.html',articles = articles, title = title) 
+    sources = get_sources()
+    return render_template('index.html',sources = sources, title = title) 
--- app/requests.py
+++ app/requests.py
@@ -16,19 +16,19 @@ def configure_request(app):
     NEWS_API = app.config['NEWS_API']
     BASE_URL = app.config['BASE_URL']
 
-def get_headlines():
+def get_sources():
     """
     This will find the major headlines
     """
-    get_headlines_url = BASE_URL.format('top-headlines',NEWS_API)
+    get_headlines_url = BASE_URL.format('top-headlines/sources',NEWS_API)
 
     with urllib.request.urlopen(get_headlines_url) as url:
         get_headline_data = url.read()
-        get_headline_response = json.loads(get_headline_data)['articles']
-        result = process_articles(get_headline_response)
+        get_headline_response = json.loads(get_headline_data)['sources']
+        result = process_sources(get_headline_response)
     return result
 
-def process_articles(result_list):
+def process_sources(result_list):
     """
     Function that processes the results into the object with the properties we require
     
@@ -37,18 +37,17 @@ def process_articles(result_list):
         
     Returns: A list of objects that only has the required properties
     """
-    articles_list = []
-    for article in result_list:
-        source = article['source']['name']
-        author = article['author']
-        title = article['title']
-        description = article['description']
-        link = article['url']
-        image_url = article['urlToImage']
-        published = article['publishedAt']
-        
-        new_article = Article(source,author,title,description,link,image_url,published)
+    sources_list = []
 
-        articles_list.append(new_article)
+    for source in result_list:
+        id = source['id']
+        name = source['name']
+        description = source['description']
+        category = source['category']
+        country = source['country']
 
-    return articles_list
+        new_source = Source(id,name,description,category,country)
+
+        sources_list.append(new_source)
+
+    return sources_list
